d30cd0b39498950a8998de73e6bdfcb595fabb91
Sai Krishna Naraharasetti <saikrishnanaraharasetti@sais-mbp.wifi.uc.edu>
2024-02-04 16:11:49 -0500

    Streamlit login changes commit.

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:3.9
+
+WORKDIR /app
+
+COPY requirements.txt ./requirements.txt
+
+COPY . ./
+
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    curl \
+    software-properties-common \
+    git \
+    && rm -rf /var/lib/apt/lists/*
+
+
+RUN pip3 install -r requirements.txt
+
+EXPOSE 80
+ENTRYPOINT ["streamlit", "run", "app.py","--server.port=80", "--server.address=0.0.0.0", "--browser.gatherUsageStats=false"]
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cloudAssign1',  # Replace with your package's name
+    version='0.1',  # Your package's version
+    description='A short description of your package',  # A brief description
+    author='Your Name',  # Your name
+    author_email='your.email@example.com',  # Your email
+    url='https://github.com/yourusername/yourpackagename',  # URL to your package's repository
+    packages=find_packages(exlude=["test", "tests"]),  # Automatically find your package
+    install_requires=[
+        'numpy',  # List your dependencies here
+        'pandas',
+        'streamlit'
+        # etc.
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',  # Specify which Python versions you support
+        'License :: OSI Approved :: MIT License',  # Your license
+        'Operating System :: OS Independent',
+        # etc.
+    ],
+    python_requires='>=3.9',  # Minimum version requirement of the package
+)
